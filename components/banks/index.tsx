b2b9528fd5da655171ec99f37758c65687d04b9e
maniruzzaman.akash <=>
2023-02-23 01:32:27 +0600

    Fixed bank modal popup

--- components/banks/index.tsx
+++ components/banks/index.tsx
@@ -164,7 +164,7 @@ export default function Banks(props: IBanksProps) {
 
             <NewBank
                showModal={showModal}
-               setShowModal={() => setShowModal(true)}
+               setShowModal={(value) => setShowModal(value)}
                changeTextInput={changeTextInput}
                onSubmit={(e) => onSubmit(e, "add")}
             />
