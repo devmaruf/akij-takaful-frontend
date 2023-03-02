a575206ee23fab327b24798f1644eac68f872473
maniruzzaman.akash <=>
2023-03-02 10:50:41 +0600

    Fixed dashboard countings

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -22,9 +22,7 @@ export default function Navbar() {
                             <svg id="toggleSidebarMobileClose" className="w-6 h-6 hidden" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
                         </button>
                         <a href="" className="text-xl font-bold flex items-center lg:ml-2.5">
-                            {/* <img src="/images/logo.svg" className="h-6 mr-2" alt="AF" /> */}
                             <Image src={'/images/banner.png'} alt={''} height={80} width={300} unoptimized />
-                            {/* <span className="self-center whitespace-nowrap text-gray-900">Akij Takaful</span> */}
                         </a>
                         <form action="#" method="GET" className="hidden lg:block lg:pl-32">
                             <label htmlFor="topbar-search" className="sr-only">Search</label>
